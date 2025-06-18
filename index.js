60f3b1e6ca28260565eaf0d99e6086f5c2ad13c8
ManuelLopezParedes <manuel.eduardo.lopeza@gmail.com>
2025-06-18 14:24:57 -0600

    prueba openpay

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { agregarAsync, obtenerTodoAsync } = require("./webhook.repositorio")
 
 app = express()
 
-app.use(express.json())
+app.use(express.json()) //obtenemos el body del req
 
 // endpoint "/api/"
 app.get("/", (req, res) => {
@@ -14,31 +14,20 @@ app.get("/", (req, res) => {
 })
 
 app.post("/api/webhook", async (req, res) => {
-    console.log("Datos recibidos:", req.body);
-
-    // 1. Verificación inicial de OpenPay (handshake)
-    if (req.body.type === 'verification') {
-        console.log("OpenPay verificando el webhook...");
-        return res.status(200).json({ 
-            challenge: req.body.challenge  // ¡OpenPay espera este campo!
-        });
+    console.log("req.body")
+    const body = req.body
+    const headers = req.headers
+    const query = req.query
+    const webhook = {
+        body: body,
+        headers: headers,
+        query: query,
+        fechaDeRegistro: new Date()
     }
-
-    // 2. Evento real (ej: pago exitoso)
-    if (req.body.type === 'charge.succeeded') {
-        const webhookData = {
-            body: req.body,
-            headers: req.headers,
-            fechaDeRegistro: new Date(),
-            paymentId: req.body.transaction.id
-        };
-        await agregarAsync(webhookData);
-        return res.status(200).json({ success: true });
-    }
-
-    // 3. Otros eventos no manejados
-    res.status(200).end();
-});
+    await agregarAsync(webhook)
+    // buscar que el pago ya este
+    res.status(201).json(webhook)
+})
 
 app.listen(3000, () => {
     console.log("http://localhost:3000")
